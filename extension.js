38f1161d42952a7e0475e32ecb2fa4c44f71cc23
pvavilov <pasha.v13@gmail.com>
2025-01-19 19:32:08 +0200

    After opening the app for the first time and the window in full-screen mode, move it to the new workspace

--- extension.js
+++ extension.js
@@ -9,9 +9,10 @@ let maximizeToWorkspace = null;
 
 const WORKSPACE_IS_UNDEFINED = -1;
 
-const STATE_MAXIMIZED = 1;
-const STATE_UNMAXIMIZED = 2;
-const STATE_CLOSED = 3;
+const STATE_OPENED = 1;
+const STATE_MAXIMIZED = 2;
+const STATE_UNMAXIMIZED = 3;
+const STATE_CLOSED = 4;
 
 function logMessage(message, alwaysLog = false) {
     if (alwaysLog || settings.enableLogs) {
@@ -25,8 +26,19 @@ function MaximizeToWorkspace() {
 
 MaximizeToWorkspace.prototype = {
     _init: function() {
+        this._openedEventID = 0;
         this._sizeChangeEventID = 0;
         this._closedEventID = 0;
+    },
+    _opened: function (shellwm, actor) {
+        if (!actor) {
+            return;
+        }
+        let window = actor.get_meta_window();
+        window._maximizeToWorkspaceState = STATE_OPENED;
+        logMessage(`opened: ${window.title} [${window.get_wm_class()}]`);
+        if (window.get_maximized() !== Meta.MaximizeFlags.BOTH) return;
+        this._maximize(shellwm, actor);
     },
     _handleResize: function(shellwm, actor, change) {
         logMessage("handle resize");
@@ -67,7 +79,10 @@ MaximizeToWorkspace.prototype = {
             return;
         }
         let window = actor.get_meta_window();
-        if (window._previousWorkspaceIndex === WORKSPACE_IS_UNDEFINED) {
+        if (
+            window._previousWorkspaceIndex === undefined
+            || window._previousWorkspaceIndex === WORKSPACE_IS_UNDEFINED
+        ) {
             return;
         }
 
@@ -76,23 +91,22 @@ MaximizeToWorkspace.prototype = {
         window._maximizeToWorkspaceState = STATE_UNMAXIMIZED;
         let currentTime = global.get_current_time();
         logMessage(`unmaximized: ${window.title} [${window.get_wm_class()}] workspace #${previousWorkspaceIndex}`);
-
-        if (targetWorkspace.list_windows().filter(w => !w.is_on_all_workspaces()).length <= 1) {
-            Mainloop.timeout_add(500, () => {
-                logMessage(`unmaximized change&remove workspace: ${window.title} [${window.get_wm_class()}]`);
-                if (
-                    window._maximizeToWorkspaceState !== STATE_UNMAXIMIZED
-                    || window._previousWorkspaceIndex === WORKSPACE_IS_UNDEFINED
-                ) {
-                    return;
-                }
-                window.change_workspace_by_index(previousWorkspaceIndex, false);
-                window.activate(currentTime);
-                global.screen.remove_workspace(targetWorkspace, currentTime);
-                window._previousWorkspaceIndex = WORKSPACE_IS_UNDEFINED;
-            });
+        if (targetWorkspace.list_windows().filter(w => !w.is_on_all_workspaces()).length > 1) {
+            return;
         }
-
+        Mainloop.timeout_add(500, () => {
+            logMessage(`unmaximized change&remove workspace: ${window.title} [${window.get_wm_class()}]`);
+            if (
+                window._maximizeToWorkspaceState !== STATE_UNMAXIMIZED
+                || window._previousWorkspaceIndex === WORKSPACE_IS_UNDEFINED
+            ) {
+                return;
+            }
+            window.change_workspace_by_index(previousWorkspaceIndex, false);
+            window.activate(currentTime);
+            global.screen.remove_workspace(targetWorkspace, currentTime);
+            window._previousWorkspaceIndex = WORKSPACE_IS_UNDEFINED;
+        });
     },
     _closed: function (shellwm, actor) {
         if (!actor) {
@@ -122,10 +136,15 @@ MaximizeToWorkspace.prototype = {
     },
     enable: function() {
         logMessage("Enable");
+        this._openedEventID = global.window_manager.connect("map", Lang.bind(this, this._opened));
         this._sizeChangeEventID = global.window_manager.connect("size-change", Lang.bind(this, this._handleResize));
         this._closedEventID = global.window_manager.connect("destroy", Lang.bind(this, this._closed));
     },
     disable: function() {
+        if (this._openedEventID) {
+            global.window_manager.disconnect(this._openedEventID);
+            this._openedEventID = 0;
+        }
         if (this._sizeChangeEventID) {
             global.window_manager.disconnect(this._sizeChangeEventID);
             this._sizeChangeEventID = 0;
